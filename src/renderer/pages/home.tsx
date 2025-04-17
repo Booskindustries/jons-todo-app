29b1cfb8258e598d81a297847088424dbcc9cca7
JonnyBHub <jonny.barrett@hotmail.com>
2025-04-17 23:58:19 +0100

    change

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
   return (
     <div>
-        <h1>Welcome to Jon's Todo App</h1>
+        <h1 style={{fontSize:20}}>Welcome to Jon's Todo App</h1>
         <button onClick={() => alert('Button clicked!')}>Click Me!</button>
         <p>This is a simple todo app built with Electron and React.</p>
         <input type="text" placeholder="Add a new task" />
