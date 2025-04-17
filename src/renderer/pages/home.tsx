ce4cf530d6cb51d6cf93ad6a9a26c0a542b9d97a
JonnyBHub <jonny.barrett@hotmail.com>
2025-04-17 23:14:50 +0100

    new version for testing auto update

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Homepage = () => {
   return (
     <div>
-      <h1>Welcome to Jon's Todo App</h1>
-      <button onClick={() => alert('Button clicked!')}>Click Me!</button>
+        <h1>Welcome to Jon's Todo App</h1>
+        <button onClick={() => alert('Button clicked!')}>Click Me!</button>
+        <p>This is a simple todo app built with Electron and React.</p>
     </div>
   );
 };
