f7235cf606c24929bdeae7963e76d3d66fdd1e0b
JonnyBHub <jonny.barrett@hotmail.com>
2025-04-19 20:57:02 +0100

    added a toast with an action - action needs to be fixed

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Homepage from './pages/home';
 import Layout from './layout';
+import { Toaster } from "@/components/ui/sonner"
 
 const App = () => {
   return (
-    <Layout>
-      <Homepage />
-    </Layout>
+    <>
+      <Layout>
+        <Homepage />
+      </Layout>
+      <Toaster />
+    </>
   );
 };
 
--- src/renderer/components/Taskitem.tsx
+++ src/renderer/components/Taskitem.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { toast } from "sonner";
+
 
 interface TaskItemProps {
     title: string;
     body: string;
 }
+//todo: add date prop for a time to finish task
+const TaskItem: React.FC<TaskItemProps> = ({ title, body }) => {
+    const [checked, setChecked] = React.useState(false);
 
-const TaskItem: React.FC<TaskItemProps> = ({ title, body, date }) => {
+    const handleCheckboxClick = () => {
+        setChecked(!checked);
+        toast(`Task "${title}" ${checked ? "unchecked" : "checked"}`, {
+            duration: 2000,
+            description: `You have ${checked ? "unchecked" : "checked"} the task "${title}".`,
+            action: {
+                label: "Undo",
+            onClick: () => {
+                    setChecked(!checked);
+                    toast.dismiss();
+                }}});
+        
+    };
+
     return (
         <div className="flex items-center mb-2.5">
-            <Checkbox className="mr-2.5" />
+            <Checkbox className="mr-2.5" onClick={handleCheckboxClick} />
             <div>
                 <h3 className="m-0 text-base font-medium">{title}</h3>
                 <p className="m-0 text-sm text-gray-600">{body}</p>
@@ -19,3 +37,4 @@ const TaskItem: React.FC<TaskItemProps> = ({ title, body, date }) => {
 };
 
 export default TaskItem;
+
