ea3d9d09ff8763e62fa5eab8f25bfa7653d0bd55
JonnyBHub <jonny.barrett@hotmail.com>
2025-04-27 01:42:19 +0100

    some placeholders on the settings

--- src/renderer/pages/settings.tsx
+++ src/renderer/pages/settings.tsx
@@ -6,10 +6,20 @@ import ThemeSelector from "@/components/ThemeSelector";
 export function Settings() {
     return (
         <div className="flex h-screen w-full flex-col items-center justify-center bg-background text-foreground">
-            <h1 className="text-2xl font-bold">Settings</h1>
+            <h1 className="text-3xl font-bold">Settings</h1>
             <p className="mt-4">This is the settings page.</p>
 
             <ThemeSelector />
+
+            <div className="mt-4 flex flex-col items-center space-y-2">
+                <h2 className="text-2xl font-bold">Fonts</h2>
+            </div>
+            <div className="mt-4 flex flex-col items-center space-y-2">
+                <h2 className="text-2xl font-bold">Text size</h2>
+            </div>
+            <div className="mt-4 flex flex-col items-center space-y-2">
+                <h2 className="text-2xl font-bold">Text size</h2>
+            </div>
         </div>
     );
 }
