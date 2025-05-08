b9fbe6bb74e8890093d2f47b78e1bbd2a5bfa4e9
JonnyBHub <jonny.barrett@hotmail.com>
2025-05-08 15:57:07 +0100

    some animations

--- src/renderer/pages/loginpage.tsx
+++ src/renderer/pages/loginpage.tsx
@@ -14,14 +14,13 @@ export default function LoginPage() {
       if (isLoggedIn) {
         navigate("/"); // Redirect to the home page if already logged in
       }
-    }, [isLoggedIn, navigate]);
-
+    }, [isLoggedIn, navigate]); 
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground  hover:transition  hover:delay-300 hover:animate-bounce">
             <GalleryVerticalEnd className="size-4" />
           </div>
           Acme Inc.
